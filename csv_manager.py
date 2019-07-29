7b55ec0b20970f063a8c379d60d7bf1f4c1f4ca5
Gabi B <Gab3009@Gmail.com>
2019-07-29 21:20:37 +0300

    for corinne

--- csv_manager.py
+++ csv_manager.py
@@ -1,5 +1,5 @@
 import csv
-
+from manipulator import Manipulator
 
 class CSV_Manager:
     def __init__(self, filename):
@@ -22,3 +22,15 @@ class CSV_Manager:
             all_data.append(data_dict)
 
         return all_data
+    def map_author(self):
+        with open(self.filename) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            rows = [row for row in csv_reader]
+        articles = self.get_csv_as_dicts()
+        manipulator = Manipulator(articles)
+        a = {}
+        b = rows.pop(0)
+        for x in range(4):
+            for j in rows:
+                a[j[x]] = manipulator.filter_by(b[x], j[x])
+        return a
--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from csv_manager import CSV_Manager
 from manipulator import Manipulator
 import json
 
-def run(filter_field, value):
+def run(filter_field, value = None):
     reader = CSV_Manager("./articles.csv")
-    articles = reader.get_csv_as_dicts()
-    manipulator = Manipulator(articles)
+    if value != None:
+        articles = reader.get_csv_as_dicts()
+        manipulator = Manipulator(articles)
 
-    filtered = manipulator.filter_by(filter_field, value)
-    print(json.dumps(filtered, indent=2))
+        filtered = manipulator.filter_by(filter_field, value)
+        print(json.dumps(filtered, indent=2))
+    else:
+        a = reader.map_author()
+        print(json.dumps(a[filter_field], indent=2))
+
+    
     
 
-run("author", "a2")
+run("a1")
+run("author", "a1")
